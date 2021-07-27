249f637bd5e6caaef1263dd87e25080398e4762e
Anderson Felipe <anderson_fpv@yahoo.com.br>
2021-07-27 14:00:27 -0300

    Criando o componente de slider

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
 
 import {
   Container,
-  Header
+  Header,
+  CarImages
 } from './styles';
 
 export function CarDetails() {
@@ -12,6 +14,13 @@ export function CarDetails() {
         <Header>
             <BackButton onPress={() => {}}/>
         </Header>
+
+        <CarImages>
+            <ImageSlider 
+                imagesUrl={['https://www.motortrend.com/uploads/sites/10/2018/05/2018-audi-rs5-4wd-coupe-angular-front.png']} 
+            />
+        </CarImages>
+
     </Container>
   );
 }
