ba054da37d36864e88d3de171a609b78866eb730
Anderson Felipe <anderson_fpv@yahoo.com.br>
2021-08-08 01:51:41 -0300

    Tela de login

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -12,7 +12,8 @@ interface Props {
   color?: string;
   onPress: () => void;
   enabled?: boolean; 
-  loading?: boolean;   
+  loading?: boolean;  
+  light?: boolean; 
 }
 
 export function Button({
@@ -21,21 +22,22 @@ export function Button({
   onPress,
   enabled = true,
   loading = false,
+  light = false,
 }: Props) {
   const theme = useTheme();
 
   return (
-    <Container 
-      color={color ? color : theme.colors.main} 
+    <Container
+      color={color ? color : theme.colors.main}
       onPress={onPress}
       enabled={enabled}
-      style={{  opacity: (enabled === false || loading === true) ? .5 : 1 }}
+      style={{ opacity: enabled === false || loading === true ? 0.5 : 1 }}
     >
-      {
-        loading 
-        ? <ActivityIndicator color={theme.colors.shape} />
-        : <Title>{title}</Title>
-      }
+      {loading ? (
+        <ActivityIndicator color={theme.colors.shape} />
+      ) : (
+        <Title light={light}>{title}</Title>
+      )}
     </Container>
   );
 }
