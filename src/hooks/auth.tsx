759e6b9334e90582b7117bfbb5f1dd54acbcb88b
Anderson Felipe <anderson_fpv@yahoo.com.br>
2021-08-15 00:08:46 -0300

    Persistindo dados

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -12,17 +12,12 @@ import { User as ModelUser } from '../database/model/User';
 
 interface User {
   id: string;
-  // user_id: string;
+  user_id: string;
   email: string;
   name: string;
   driver_license: string;
   avatar: string;
-  // token: string;
-}
-
-interface AuthState {
   token: string;
-  user: User;
 }
 
 interface SignInCredentials {
@@ -44,45 +39,35 @@ interface AuthProviderProps {
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 function AuthProvider({ children } : AuthProviderProps) {
-  const [data, setData] = useState<AuthState>({} as AuthState);
+  const [data, setData] = useState<User>({} as User);
 
-  async function signIn({ email, password } : SignInCredentials) {
-    const response = await api.post("/sessions", {
-      email,
-      password,
-    });
-
-    const { token, user } = response.data;
-    api.defaults.headers.authorization = `Bearer ${token}`;
-    setData({ token, user });
-    
-    // try{
-    //   const response = await api.post('/sessions', {
-    //     email,
-    //     password
-    //   });
+  async function signIn({ email, password } : SignInCredentials) {   
+    try{
+      const response = await api.post('/sessions', {
+        email,
+        password
+      });
   
-    //   const { token, user } = response.data;
-    //   api.defaults.headers.authorization = `Bearer ${token}`;
-    //   setData({ token, user });
+      const { token, user } = response.data;
+      api.defaults.headers.authorization = `Bearer ${token}`;
 
-      // const userCollection = database.get<ModelUser>('users');
-      // await database.action(async () => {
-      //   await userCollection.create(( newUser ) => {
-      //     newUser.user_id = user.id,
-      //     newUser.name = user.name,
-      //     newUser.email = user.email,
-      //     newUser.driver_license = user.driver_license,
-      //     newUser.avatar = user.avatar,
-      //     newUser.token = token
-      //   })
-      // });
+      const userCollection = database.get<ModelUser>('users');
+      await database.action(async () => {
+        await userCollection.create(( newUser ) => {
+          newUser.user_id = user.id,
+          newUser.name = user.name,
+          newUser.email = user.email,
+          newUser.driver_license = user.driver_license,
+          newUser.avatar = user.avatar,
+          newUser.token = token
+        })
+      });
   
-      // setData({ ...user, token });
+      setData({ ...user, token });
 
-    // }catch(error){
-    //   throw new Error(error);
-    // }
+    }catch(error){
+      throw new Error(error);
+    }
   }
 
   // async function signOut() {
@@ -118,25 +103,25 @@ function AuthProvider({ children } : AuthProviderProps) {
   //   }
   // }
 
-  // useEffect(() => {
-  //   async function loadUserData() {
-  //     const userCollection = database.get<ModelUser>('users');
-  //     const response = await userCollection.query().fetch();
+  useEffect(() => {
+    async function loadUserData() {
+      const userCollection = database.get<ModelUser>('users');
+      const response = await userCollection.query().fetch();
      
-  //     if(response.length > 0){
-  //       const userData = response[0]._raw as unknown as User;
-  //       api.defaults.headers.authorization = `Bearer ${userData.token}`;
-  //       setData(userData);
-  //     }
-  //   }
+      if(response.length > 0){
+        const userData = response[0]._raw as unknown as User;
+        api.defaults.headers.authorization = `Bearer ${userData.token}`;
+        setData(userData);
+      }
+    }
 
-  //   loadUserData();
-  // },[])
+    loadUserData();
+  },[])
 
   return (
     <AuthContext.Provider 
       value={{
-        user: data.user,
+        user: data,
         signIn,
         // signOut,
         // updatedUser
