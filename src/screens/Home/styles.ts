46022cd39af1383604c6b85c05eae1c36a6a47d5
Anderson Felipe <anderson_fpv@yahoo.com.br>
2021-08-15 22:54:26 -0300

    Sicronizando usuário

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,7 +1,7 @@
 import { RFValue } from 'react-native-responsive-fontsize';
 import { FlatList } from 'react-native';
 import styled from 'styled-components/native';
-import { CarDTO } from "../../dtos/CarDTO";
+import { Car } from "../../database/model/Car";
 
 export const Container = styled.View`
     flex: 1;
@@ -31,7 +31,7 @@ export const TotalCars = styled.Text`
   color: ${({ theme }) => theme.colors.text};
 `;
 
-export const CarList = styled(FlatList as new () => FlatList<CarDTO>).attrs({
+export const CarList = styled(FlatList as new () => FlatList<Car>).attrs({
     contentContainerStyle: {
         padding: 24
     },
